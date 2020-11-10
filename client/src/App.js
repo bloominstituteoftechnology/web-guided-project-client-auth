14ee9307ee5416a352122d9047952576813923ae
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-10 10:33:07 -0500

    adds logout link

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,23 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import GasPrices from './components/GasPrices';
 
+import axios from 'axios';
+
 function App() {
+  const logout = () => {
+    
+  };
+
+
   return (
     <Router>
       <div className="App">
         <ul>
           <li>
             <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <a onClick={logout}>Logout</a>
           </li>
           <li>
             <Link to="/protected">Protected Page</Link>
